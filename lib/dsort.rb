ae4aabe9c6463d9ad5d37e34da4c699b66c0ca7a
Claus Rasmussen <clr@veda.dk>
2017-08-11 07:50:11 +0200

    More flexible dependency pairs

--- lib/dsort.rb
+++ lib/dsort.rb
@@ -5,78 +5,71 @@ require 'tsort'
 module DSort
   # dsort sort its input in "dependency" order: The input can be though of
   # depends-on relations between objects and the output as sorted in the order
-  # needed to safisfy those depends-on relations. Three formats can be used:
+  # needed to safisfy those depends-on relations
   #
-  # dsort [[:a, :b], [:b, :c]]
-  #     array of pairs where the first element comes before the second
-  # dsort [[:a, [:b, :c, :d]], [:b, [:c, :d]]
-  #     array of pairs where the first element comes before all the elements in
-  #     the second array
-  # dsort([obj1, obj2, obj3]) { |obj| obj.followers }
-  #     array of objects that are given to the block. The block should return a
-  #     list of elements that comes after the given object
+  # Input can be an array of relations where each relation is a pair (array) of
+  # an element and the element(s) it depends on: [[:a, :b], [:a, :c]] or [[:a,
+  # [:b, :c]]]. Note that if your elements are arrays then you need to put the
+  # second element in an array even if there's only one like this [array1,
+  # [array2]]. If a block is given, the arguments should be an array of objects
+  # and the block return an array of objects it depends on
   #
-  # An example: If we have that dsort depends on ruby and rspec, ruby depends
+  # Example: If we have that dsort depends on ruby and rspec, ruby depends
   # on C to compile, and rspec depends on ruby, then in what order should we
   # build them ? Using dsort we could do
   #
   #   require 'dsort'
-  #   p dsort [[:dsort, :ruby], [:dsort, :rspec], [:ruby, :C], [:rspec, :ruby]]
+  #   p dsort [[:dsort, :ruby, :rspec]], [:ruby, :C], [:rspec, :ruby]]
   #       => [:C, :ruby, :rspec, :dsort]
   #
-  def dsort(a, &block) Private::DSortObject.new(a, &block).tsort end
+  def dsort(a, &block) DSortPrivate::DSortObject.new(a, &block).tsort end
 
   # tsort sort its input in topological order: The input can be thought of as
   # comes-before relations between objects and the output will be in
   # first-to-last order. This definition corresponds to the mathemacial
   # defitionnn of topological sort. See
-  # http://en.wikipedia.org/wiki/Topological_sorting. Three formats can be
-  # used:
+  # http://en.wikipedia.org/wiki/Topological_sorting
   #
-  # tsort [[:a, :b], [:b, :c]]
-  #     array of pairs where the first element comes before the second
-  # tsort [[:a, [:b, :c, :d]], [:b, [:c, :d]]
-  #     array of pairs where the first element comes before all the elements in
-  #     the second array
-  # tsort([obj1, obj2, obj3]) { |obj| obj.followers }
-  #     array of objects that are given to the block. The block should return a
-  #     list of elements that comes after the given object
-  #     
-  #   
-  #
+  # Input can be an array of relations where each relation is a pair (array) of
+  # an element and the element(s) it precedes: [[:a, :b], [:a, :c]] or [[:a,
+  # [:b, :c]]]. Note that if your elements are arrays then you need to put the
+  # second argument in an array even if there's only one like this [array1,
+  # [array2]]. If a block is given, the arguments should be an array of objects
+  # and the block return an array of succeeding objects
   #
   def tsort(a, &block) dsort(a, &block).reverse end
 
   module_function :dsort, :tsort
 
-  module Private
+  module DSortPrivate
     class DSortObject
       include TSort
 
+      # Hash from element to array of dependencies
       attr_reader :deps
 
       # Create @deps hash from object to list of dependencies
       def initialize(a, &block)
         @deps = {}
-        
-        # Array of objects
         if block_given?
           a.each { |obj| @deps[obj] = block.call(obj) }
-
-        # Array of pairs
-        elsif !a.empty? && a.first.is_a?(Array) && a.first.last.is_a?(Array)
-          a.each { |k, ds| (@deps[k] ||= []).concat(ds) }
-
-        # Array of object and object dependencies pairs
         else
-          a.each { |e| (@deps[e.first] ||= []) << e.last }
+          a.each { |obj, deps| 
+            (@deps[obj] ||= []).concat(deps.is_a?(Array) ? deps : [deps])
+          }
         end
 
         # Make sure all dependent objects are also represented as depending
-        # objects
-        @deps.values.flatten.each { |obj| @deps[obj] = [] if !@deps.key?(obj) }
+        # objects: If we're given [[:a, :b]] we want the @deps hash to include
+        # both :a and :b as keys
+        @deps.values.each { |deps|
+          (deps.is_a?(Array) ? deps : [deps]).each { |d|
+            @deps[d] = [] if !@deps.key?(d)
+          }
+        }
       end
 
+      # TSort virtual methods
       def tsort_each_node(&block) @deps.each_key(&block) end
       def tsort_each_child(node, &block) @deps[node].each(&block) end
     end
--- spec/dsort_spec.rb
+++ spec/dsort_spec.rb
@@ -6,30 +6,33 @@ describe DSort do
   describe "#dsort" do
     context "with dependency pairs" do
       it "should do a dependency sort" do
+        pairs = [[:a, :b], [:b, :c]]
+        DSort.dsort(pairs).should == [:c, :b, :a]
+      end
+      it "should accept self-dependencies" do
         pairs = [[:a, :b], [:b, :c], [:c, :c]]
         DSort.dsort(pairs).should == [:c, :b, :a]
       end
       it "should accept duplicate keys" do
-        pairs = [[:a, :b], [:b, :c], [:a, :c], [:c, :c]]
+        pairs = [[:a, :b], [:b, :c], [:a, :c]]
         DSort.dsort(pairs).should == [:c, :b, :a]
       end
-      it "should accept 'leaf' dependencies" do
-        pairs = [[:a, :b], [:b, :c]]
+      it "should accept duplicate dependencies" do
+        pairs = [[:a, :b], [:a, :b], [:b, :c], [:a, :c]]
         DSort.dsort(pairs).should == [:c, :b, :a]
       end
-    end
-    context "with object to array of dependencies pairs" do
-      it "should do a dependency sort" do
-        pairs = [[:a, [:b, :c]], [:b, [:c]], [:c, [:c]]]
+      it "should accept lists of dependencies" do
+        pairs = [ [:a, [:b, :c]], [:b, [:c]] ]
         DSort.dsort(pairs).should == [:c, :b, :a]
       end
-      it "should accept duplicate keys" do
-        pairs = [[:a, [:b]], [:b, [:c]], [:a, [:c]], [:c, [:c]]]
+      it "should accept empty lists of dependencies" do
+        pairs = [ [:a, [:b, :c]], [:b, [:c]], [:c, []] ]
         DSort.dsort(pairs).should == [:c, :b, :a]
       end
-      it "should accept 'leaf' dependencies" do
-        pairs = [[:a, [:b, :c]], [:b, [:c]]]
-        DSort.dsort(pairs).should == [:c, :b, :a]
+      it "should accept arrays as values" do
+        a, b, c = [:a], [:b], [:c]
+        pairs = [ [a, [b, c]], [b, [c]] ]
+        DSort.dsort(pairs).should == [c, b, a]
       end
     end
     context "with objects and a block" do
@@ -38,17 +41,32 @@ describe DSort do
         DSort.dsort(objs) { |obj| [obj.superclass].compact }.should == 
             [BasicObject, Object, Numeric, Integer, Float]
       end
-      it "should accept 'leaf' dependencies" do
-        objs = [Integer, Numeric, Float, Object, BasicObject]
-        DSort.dsort(objs) { |obj| [obj.superclass] }.should == 
-            [nil, BasicObject, Object, Numeric, Integer, Float]
-      end
     end
   end
   describe "#tsort" do
-    it "should be equivalent to DSort::dsort(...).reverse" do
-      pairs = [[:a, :b], [:b, :c]]
-      DSort.tsort(pairs).should == [:c, :b, :a].reverse
+    context "with dependency pairs" do
+      it "should be equivalent to DSort::dsort(...).reverse" do
+        a, b, c = [:a], [:b], [:c]
+        test_pairs = [ # From the dsort tests above
+          [[:a, :b], [:b, :c]],
+          [[:a, :b], [:b, :c], [:c, :c]],
+          [[:a, :b], [:b, :c], [:a, :c]],
+          [[:a, :b], [:a, :b], [:b, :c], [:a, :c]],
+          [ [:a, [:b, :c]], [:b, [:c]] ],
+          [ [:a, [:b, :c]], [:b, [:c]], [:c, []] ],
+          [ [a, [b, c]], [b, [c]] ]
+        ]
+        test_pairs.each { |pairs|
+          DSort.tsort(pairs).should == DSort.dsort(pairs).reverse
+        }
+      end
+    end
+    context "with objects and a block" do
+      it "should be equivalent to DSort::dsort(...).reverse" do
+        objs = [Integer, Numeric, Float, Object, BasicObject]
+        DSort.tsort(objs) { |obj| [obj.superclass].compact }.should == 
+            DSort.dsort(objs) { |obj| [obj.superclass].compact }.reverse
+      end
     end
   end
 end
